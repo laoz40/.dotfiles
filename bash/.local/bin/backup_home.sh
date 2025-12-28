777fca540f97b3b84b34ccd82231fa245e61d3b6
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-12-28 11:50:42 +1100

    show file and dir count, 10gb storage check, add more excludes

--- bash/.local/bin/backup_home.sh
+++ bash/.local/bin/backup_home.sh
@@ -7,8 +7,24 @@ current_date=$(date +%Y-%m-%d_%H-%M-%S)
 input=/home/$user
 output_dir=/home/$user/backups
 output=/${output_dir}/${user}_home_${current_date}.tar.gz
+function total_files {
+        find $1 -type f | wc -l
+}
+function total_directories {
+        find $1 -type d | wc -l
+}
+available_space=$(df "$output_dir" | awk 'NR==2 {print $4}')
+# Warn if less than 10GB (10485760 KB) free
+if [ "$available_space" -lt 10485760 ]; then
+    echo "WARNING: Low disk space. Proceed with caution."
+fi
 
 echo "Starting backup. This will take a while..."
+
+echo -n "Files to be included:"
+total_files $input
+echo -n "Directories to be included:"
+total_directories $input
 
 # Check if pigz is installed
 if ! command -v pigz &> /dev/null; then
@@ -24,7 +40,12 @@ tar -I pigz -cf $output \
 	--exclude="Downloads" \
 	--exclude=".config/chromium" \
 	--exclude=".zen" \
-	--exclude=".local/lib/node_modules/" \
+	--exclude=".local/share/Steam" \
+	--exclude=".npm" \
+	--exclude="node_modules" \
+	--exclude="target" \
+	--exclude=".cargo/registry" \
+	--exclude=".var/app" \
 	$input
 
 if [ $? -eq 0 ]; then
