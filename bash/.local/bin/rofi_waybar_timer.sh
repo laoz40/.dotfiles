c7e0e0b59fb942a03928c4ded5d5e4267be18eb1
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2025-12-31 03:00:21 +1100

    update timer script

--- bash/.local/bin/rofi_waybar_timer.sh
+++ bash/.local/bin/rofi_waybar_timer.sh
@@ -3,6 +3,7 @@
 timer_file=/tmp/waybar_timer
 timer_paused=/tmp/timer_paused
 
+# waybar read timer
 if [[ $1 == "get" ]]; then
 	if [[ -f $timer_file ]]; then
 		content=$(cat $timer_file)
@@ -18,42 +19,56 @@ if [[ $1 == "get" ]]; then
 	exit 0
 fi
 
+# waybar pause/resume on-click
 if [[ $1 == "toggle" ]]; then
-    if [[ -f $timer_file ]]; then
-        if [[ -f $timer_paused ]]; then
-            rm "$timer_paused"
-        else
-            touch "$timer_paused"
-        fi
-    fi
-    exit 0
+	if [[ -f $timer_file ]]; then
+		if [[ -f $timer_paused ]]; then
+			rm "$timer_paused"
+		else
+			touch "$timer_paused"
+		fi
+	fi
+	exit 0
 fi
 
 start_timer() {
-	local seconds=$(awk "BEGIN {print int($1 * 60)}")
-	while [ $seconds -gt 0 ]; do
-		formatted_time=$(printf "%02d:%02d" $((seconds/60)) $((seconds%60)))
-		# Repeat paused time each second
+	local duration_seconds=$(awk "BEGIN {print int($1 * 60)}")
+	local current_time=$(date +%s)
+	local end_time=$(( current_time + duration_seconds ))
+
+	touch $timer_file
+
+	while [[ -f $timer_file ]]; do
+		local current_time=$(date +%s)
+		local remaining_time=$(( end_time - current_time ))
+
 		if [[ -f $timer_paused ]]; then
-			echo $formatted_time > $timer_file
-			sleep 1
-			continue
+			# Add a second to paused time each second to maintain duration
+			((end_time++))
+		else
+			if [ $remaining_time -le 0 ]; then
+				break
+			fi
 		fi
+
+		local formatted_time=$(printf "%02d:%02d" $((remaining_time/60)) $((remaining_time%60)))
 		echo $formatted_time > $timer_file
 		sleep 1
-		((seconds--))
 	done
-	echo "Done" > $timer_file
-	notify-send "Time is up!" "Go do the thing you were supposed to do." -i alarm-clock -u critical
-	sleep 10
-	rm $timer_file
+
+	if [[ -f $timer_file ]]; then
+		echo "Done" > $timer_file
+		notify-send "Time is up!" "Go do the thing you were supposed to do." -i alarm-clock -u critical
+		sleep 10
+		rm $timer_file
+	fi
 }
 
 pause_option="Pause"
 if [[ -f $timer_paused ]]; then
-    pause_option="Resume"
+	pause_option="Resume"
 else
-    pause_option="Pause"
+	pause_option="Pause"
 fi
 
 input=$(rofi -dmenu -p "Set Timer:" <<EOF
@@ -65,32 +80,24 @@ EOF
 )
 
 if [[ $input == $pause_option ]]; then
-    if [[ -f $timer_paused ]]; then
-        rm $timer_paused
-        notify-send "Timer Resumed" -i alarm-clock -u normal
-    else
-        touch $timer_paused
-        notify-send "Timer Paused" -i alarm-clock -u normal
-    fi
-    exit 0
+	if [[ -f $timer_paused ]]; then
+		rm $timer_paused
+		notify-send "Timer Resumed" -i alarm-clock -u normal
+	else
+		touch $timer_paused
+		notify-send "Timer Paused" -i alarm-clock -u normal
+	fi
+	exit 0
 elif [[ $input == "Cancel Timer" ]]; then
 	rm $timer_file
 	notify-send "Timer Cancelled" -i alarm-clock -u normal
-	# pkill current file
-	pkill -f $(basename $0)
 	exit 0
 # If input is numbers followed by space min (optional)
 elif [[ $input =~ ^([0-9]*\.?[0-9]+)([[:space:]]*min)?$ ]]; then
 	rm $timer_paused
-	# Find all PIDs with the script name, but exclude the current PID ($$)
-	OTHER_PIDS=$(pgrep -f "$(basename "$0")" | grep -v "^$$$")
-	if [ -n "$OTHER_PIDS" ]; then
-		echo "$OTHER_PIDS" | xargs kill
-	fi
-	# Extract just the number (first parenthesis)
 	minutes=${BASH_REMATCH[1]}
-	start_timer $minutes &
+	start_timer $minutes
 	exit 0
 else
-    exit 1
+	exit 1
 fi
