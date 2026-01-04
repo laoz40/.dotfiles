a742462c28cf33a0f241dcb1617491c6ee258a9c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-04 18:44:21 +1100

    update lsp.lua

--- nvim/.config/nvim/lua/plugins/functional/lsp.lua
+++ nvim/.config/nvim/lua/plugins/functional/lsp.lua
@@ -1,18 +1,44 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
-		config = function()
-			vim.lsp.enable({
-				"lua_ls",
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"eslint",
-				"marksman",
-				"bashls"
-			})
+		dependencies = { "mason-org/mason.nvim" },
+		opts = {
+			servers = {
+
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+						},
+					},
+				},
+
+				ts_ls = {
+					on_attach = function(client, bufnr)
+						require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
+					end,
+				},
+
+				html = {},
+				cssls = {},
+				tailwindcss = {},
+				eslint = {},
+				marksman = {},
+				bashls = {},
+			},
+		},
+
+		config = function(_, opts)
+			require("mason").setup()
+			for server, config in pairs(opts.servers) do
+				vim.lsp.config(server, config)
+				vim.lsp.enable(server)
+			end
 		end,
 	},
 }
-
