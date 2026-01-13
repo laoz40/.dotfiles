59c7795bc488258c171603d872e0eb4f20ebbf6c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-14 04:16:09 +1100

    fixed wallpaper not working with symlinks

--- bash/.local/bin/change_wallpapers.sh
+++ bash/.local/bin/change_wallpapers.sh
@@ -2,7 +2,14 @@
 
 WALLPAPER_DIRECTORY=~/Pictures/Wallpapers
 
-WALLPAPER=$(find "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
+SELECTED_FILE=$(find -L "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
+
+WALLPAPER=$(realpath "$SELECTED_FILE")
+
+if [ -z "$WALLPAPER" ]; then
+    echo "No wallpaper found!"
+    exit 1
+fi
 
 hyprctl hyprpaper preload "$WALLPAPER"
 hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
