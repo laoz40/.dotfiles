dd64951efdd12161326e0717a40ed85b1b8f331b
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-15 02:17:10 +1100

    markdown preview

--- nvim/.config/nvim/lua/plugins/cosmetic/markdown.lua
+++ nvim/.config/nvim/lua/plugins/cosmetic/markdown.lua
@@ -12,4 +12,12 @@ return {
 			})
 		end,
 	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 }
