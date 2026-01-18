55e81c2e73916167a5f282c8cfaaa047ce505464
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-18 12:22:57 +1100

    grep and loop all monitors for changing wallpapers

--- bash/.local/bin/change_wallpapers.sh
+++ bash/.local/bin/change_wallpapers.sh
@@ -2,17 +2,21 @@
 
 WALLPAPER_DIRECTORY=~/Pictures/Wallpapers
 
-SELECTED_FILE=$(find -L "$WALLPAPER_DIRECTORY" -type f | shuf -n 1)
+SELECTED_FILE=$(find -L "$WALLPAPER_DIRECTORY" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.webp" \) | shuf -n 1)
 
 WALLPAPER=$(realpath "$SELECTED_FILE")
 
 if [ -z "$WALLPAPER" ]; then
-    echo "No wallpaper found!"
+    notify-send "No wallpapers found" -u critical
     exit 1
 fi
 
 hyprctl hyprpaper preload "$WALLPAPER"
-hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
+MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
+
+for MONITOR in $MONITORS; do             
+	hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
+done
 
 sleep 1
 
