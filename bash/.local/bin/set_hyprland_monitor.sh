1ad53444f6cf5a6c201be73e7ca89476818e2be4
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-23 21:33:38 +1100

    fixed wrong file names from last commit

--- bash/.local/bin/set_hyprland_monitor.sh
+++ bash/.local/bin/set_hyprland_monitor.sh
@@ -4,9 +4,9 @@ EXTERNAL_MONITOR="DP-3"
 INTERNAL_MONITOR="eDP-1"
 
 if hyprctl monitors | grep -q "$EXTERNAL_MONITOR"; then
-    echo "\$MONITOR = $EXTERNAL_MONITOR" > ~/.config/hypr/monitor.conf
+    echo "\$MONITOR = $EXTERNAL_MONITOR" > ~/.config/hypr/set_monitor.conf
 else
-    echo "\$MONITOR = $INTERNAL_MONITOR" > ~/.config/hypr/monitor.conf
+    echo "\$MONITOR = $INTERNAL_MONITOR" > ~/.config/hypr/set_monitor.conf
 fi
 
 hyprctl reload
