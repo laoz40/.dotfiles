6eeed55ddb47746f49d89af362c6e85b22b98010
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-25 03:20:54 +1100

    added sleep to power menu

--- bash/.local/bin/rofi_power_menu.sh
+++ bash/.local/bin/rofi_power_menu.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-chosen=$(printf "  Power Off\n  Reboot\n  Lock" | rofi -dmenu -i -p "Power Menu:" -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Reboot\n󰤄  Sleep\n  Lock" | rofi -dmenu -i -p "Power Menu:" -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
 	"  Reboot") reboot ;;
+	"󰤄  Sleep") systemctl suspend ;;
 	"  Lock") hyprlock ;;
 	*) exit 1 ;;
 esac
