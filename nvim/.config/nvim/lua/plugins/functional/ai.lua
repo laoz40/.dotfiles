232e34907fd6c3582b37450b2a9b53b1f34ee367
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-26 18:46:27 +1100

    added copilot (not used) to setup copilot auth to use wtf plugin

--- nvim/.config/nvim/lua/plugins/functional/ai.lua
+++ nvim/.config/nvim/lua/plugins/functional/ai.lua
@@ -15,8 +15,13 @@ return {
 		config = function()
 			require("supermaven-nvim").setup({
 				disable_inline_completion = false, -- disables inline completion for use with cmp
+				disable_default_keymaps = true, -- disables default keymaps
 				log_level = "off",
 			})
 		end,
 	},
+	-- {
+	-- 	-- to set up auth for copilot
+	-- 	"github/copilot.vim",
+	-- }
 }
