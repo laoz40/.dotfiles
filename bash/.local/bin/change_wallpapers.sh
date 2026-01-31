7ae54b0ea8cd7071380c9515dd5cbd88e22d0b36
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-01-31 13:46:17 +1100

    wallpaper picker rofi

--- bash/.local/bin/change_wallpapers.sh
+++ bash/.local/bin/change_wallpapers.sh
@@ -13,7 +13,21 @@ fi
 
 MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
 
+change_wallpaper() {
 for MONITOR in $MONITORS; do
 	hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
 	notify-send "Wallpaper changed to $(basename $WALLPAPER)" -u low
 done
+}
+
+if [[ $1 == "get_random" ]]; then
+	change_wallpaper
+	exit 0
+fi
+
+selection=$(ls "$WALLPAPER_DIRECTORY" | grep -E "\.(jpg|jpeg|png|webp)$" | rofi -dmenu -i -p "Select Wallpaper:")
+
+if [[ -n $selection ]]; then
+	WALLPAPER=$(realpath "$WALLPAPER_DIRECTORY/$selection")
+	change_wallpaper
+fi
