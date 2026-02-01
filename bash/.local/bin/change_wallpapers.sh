5d2fa87695b9f268367fd0c1ee0295b4ed243e7d
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-02-02 00:37:02 +1100

    fixed and refactored change_wallpapers.sh

--- bash/.local/bin/change_wallpapers.sh
+++ bash/.local/bin/change_wallpapers.sh
@@ -1,33 +1,37 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIRECTORY=~/Pictures/Wallpapers
+WALLPAPER_DIRECTORY=$HOME/Pictures/Wallpapers
 
-SELECTED_FILE=$(find -L "$WALLPAPER_DIRECTORY" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.webp" \) | shuf -n 1)
+find_wallpapers() {
+    find -L "$WALLPAPER_DIRECTORY" -type f \( \
+        -iname "*.jpg"  -o \
+        -iname "*.jpeg" -o \
+        -iname "*.png"  -o \
+        -iname "*.webp" \
+    \) "$@"
+}
 
-WALLPAPER=$(realpath "$SELECTED_FILE")
-
-if [ -z "$WALLPAPER" ]; then
-    notify-send "No wallpapers found" -u critical
+if ! find_wallpapers | read -r _; then
+    notify-send "No wallpapers found in $WALLPAPER_DIRECTORY" -u critical
     exit 1
 fi
 
-MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
-
-change_wallpaper() {
-for MONITOR in $MONITORS; do
-	hyprctl hyprpaper wallpaper "$MONITOR,$WALLPAPER"
-	notify-send "Wallpaper changed to $(basename $WALLPAPER)" -u low
-done
+set_wallpaper() {
+	local wallpaper="$1"
+	hyprctl hyprpaper wallpaper ",$wallpaper"
+	notify-send "Wallpaper changed to $(basename "$wallpaper")" -u low
 }
 
 if [[ $1 == "get_random" ]]; then
-	change_wallpaper
+	wallpaper=$(find_wallpapers -printf "%f\n" | shuf -n 1)
+	set_wallpaper "$(realpath "$WALLPAPER_DIRECTORY/$wallpaper")"
 	exit 0
 fi
 
-selection=$(ls "$WALLPAPER_DIRECTORY" | grep -E "\.(jpg|jpeg|png|webp)$" | rofi -dmenu -i -p "Select Wallpaper:")
+selection=$(find_wallpapers -printf "%f\n" \
+	| sort \
+	| rofi -dmenu -i -p "Select Wallpaper:"
+)
 
-if [[ -n $selection ]]; then
-	WALLPAPER=$(realpath "$WALLPAPER_DIRECTORY/$selection")
-	change_wallpaper
-fi
+[[ -n "$selection" ]] || exit 0
+set_wallpaper "$(realpath "$WALLPAPER_DIRECTORY/$selection")"
