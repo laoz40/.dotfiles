13254f043bc27558c0369c68c439a9307b6833a2
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-02-02 04:26:19 +1100

    fixed no input error, changed some error messages

--- bash/.local/bin/rofi_define.sh
+++ bash/.local/bin/rofi_define.sh
@@ -2,26 +2,21 @@
 
 # https://github.com/BreadOnPenguins/scripts/blob/master/shortcuts-menus/define
 
-word=$(echo "Use Clipboard" | rofi -dmenu -i -no-fixed-num-lines \
+word=$(echo "Use Clipboard" | rofi -dmenu -no-fixed-num-lines \
   -theme-str 'window {width: 20%; }'\
   -p "Define:"
 )
 
 [[ "$word" == "Use Clipboard" ]] && word=$(xclip -o -selection primary 2>/dev/null || wl-paste 2>/dev/null)
 
-# Check for empty word or special characters
-[[ -z "$word" || "$word" =~ [\/] ]] && notify-send -u critical -t 3000 "Invalid input." && exit 0
+[[ -z "$word" ]] && exit 0
+[[ "$word" =~ [\/] ]] && notify-send -u critical -t 3000 "Invalid input." && exit 0
 
 query=$(curl -s --connect-timeout 5 --max-time 10 "https://api.dictionaryapi.dev/api/v2/entries/en_US/$word")
 
-# Check for connection error (curl exit status stored in $?)
-[ $? -ne 0 ] && notify-send -u critical -t 3000 "Connection error." && exit 1
-
-# Check for invalid word response
+[ $? -ne 0 ] && notify-send -u critical -t 3000 "Invalid input." && exit 0
 [[ "$query" == *"No Definitions Found"* ]] && notify-send -u critical -t 3000 "Invalid word." && exit 0
 
 # Show first definition for each part of speech (thanks @morgengabe1 on youtube)
 def=$(echo "$query" | jq -r '.[0].meanings[] | "\(.partOfSpeech): \(.definitions[0].definition)\n"')
-
-# Requires a notification daemon to be installed
-notify-send "$word" "$def"
+notify-send -t 20000 "$word" "$def"
