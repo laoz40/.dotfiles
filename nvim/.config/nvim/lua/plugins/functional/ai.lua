b020a36e6afaee72781fb174d7c077e04bb9226c
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-02-02 00:53:27 +1100

    opencode.nvim

--- nvim/.config/nvim/lua/plugins/functional/ai.lua
+++ nvim/.config/nvim/lua/plugins/functional/ai.lua
@@ -24,4 +24,33 @@ return {
 	-- 	-- to set up auth for copilot
 	-- 	"github/copilot.vim",
 	-- }
+	{
+		"NickvanDyke/opencode.nvim",
+		dependencies = {
+			{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+		},
+		config = function()
+			vim.g.opencode_opts = {
+				provider = {
+					enabled = "tmux",
+					tmux = {},
+				},
+			}
+			vim.o.autoread = true
+			vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+				require("opencode").ask("@this: ", { submit = true })
+			end, { desc = "Ask opencode…" })
+
+			vim.keymap.set({ "n", "x" }, "<leader>ox", function()
+				require("opencode").select()
+			end, { desc = "Select opencode action" })
+
+			vim.keymap.set({ "n", "x" }, "go", function()
+				return require("opencode").operator("@this ")
+			end, { desc = "Add range to opencode", expr = true })
+			vim.keymap.set("n", "goo", function()
+				return require("opencode").operator("@this ") .. "_"
+			end, { desc = "Add line to opencode", expr = true })
+		end,
+	},
 }
