6b2cee2f274f20515c94928877c6b9cd7e74fb8d
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-02-03 03:44:31 +1100

    BIG REFACTORS

--- bash/.local/bin/rofi_cliphist.sh
+++ bash/.local/bin/rofi_cliphist.sh
@@ -1,32 +1,27 @@
 #!/usr/bin/env bash
 
-clear_cmd=$(printf "0\tClear Clipboard History")
-
-result=$((cliphist list && echo "$clear_cmd") | rofi -dmenu -display-columns 2 -p "Clipboard:" -kb-custom-1 "Delete" -kb-remove-char-forward "")
+result=$( (cliphist list && printf "0\tClear Clipboard History") | \
+	rofi -dmenu -display-columns 2 -p "Clipboard:" -kb-custom-1 "Delete" -kb-remove-char-forward "")
 
 exit_code=$?
 
-if [ -z "$result" ]; then
-    exit 0
-fi
+[[ -z "$result" ]] && exit 0
 
 if [[ "$result" == *"Clear Clipboard History"* ]]; then
-    confirm=$(echo -e "No\nYes" | rofi -dmenu -p "Are you sure you want to wipe history?")
-    if [ "$confirm" = "Yes" ]; then
-        cliphist wipe
-    fi
-    exit 0
+	confirm=$(echo -e "No\nYes" | rofi -dmenu -p "Are you sure you want to wipe history?")
+	[[ "$confirm" = "Yes" ]] && cliphist wipe
+	exit 0
 fi
 
 case $exit_code in
-    0)
-        echo "$result" | cliphist decode | wl-copy
-        ;;
-    10)
-        echo "$result" | cliphist delete
-        exec "$0"
-        ;;
-    *)
-        exit 0
-        ;;
+	0)
+		echo "$result" | cliphist decode | wl-copy
+		;;
+	10)
+		echo "$result" | cliphist delete
+		exec "$0"
+		;;
+	*)
+		exit 0
+		;;
 esac
