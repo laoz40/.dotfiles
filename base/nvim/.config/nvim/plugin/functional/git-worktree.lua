6832bfb446e5e150d5c248eec704405d48801c94
Leo Zhou <128760280+laoz40@users.noreply.github.com>
2026-05-06 18:05:23 +1000

    added auto_switch for git-worktree.nvim

--- base/nvim/.config/nvim/plugin/functional/git-worktree.lua
+++ base/nvim/.config/nvim/plugin/functional/git-worktree.lua
@@ -2,4 +2,4 @@ vim.pack.add({
 	{ src = "https://github.com/laoz40/git-worktree.nvim" },
 })
 
-require("git-worktree").setup({ auto_install = true })
+require("git-worktree").setup({ auto_install = true, auto_switch = false })
